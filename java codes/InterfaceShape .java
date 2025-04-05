e381ec974650becf2ec64cc315a56067fb8ed83b
siripikusumareddy <siripikusumareddy@gmail.com>
2025-04-05 15:57:41 +0530

    Create InterfaceShape .java

---
+++ java codes/InterfaceShape .java
@@ -0,0 +1,47 @@
+interface Shape {
+    double area();
+    double perimeter();
+}
+
+class Circle implements Shape {
+    double radius;
+
+    Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    double length, width;
+
+    Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+
+public class InterfaceShape {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 6);
+
+        System.out.println("Circle Area: " + circle.area());
+        System.out.println("Rectangle Perimeter: " + rectangle.perimeter());
+    }
+}
